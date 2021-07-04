0230622783c803ef7cdf5288cd4cb8f0b5efe6dd
Ivan Staroselskiy <i.staroselskiy@rt-solar.ru>
2021-07-04 22:46:33 +0300

    rm infinite loop

--- sync.py
+++ sync.py
@@ -9,14 +9,11 @@ if __name__ == '__main__':
     """
     Main runner. For each Exchange account, process events every 30 minutes
     """
-    while True:
-        for exchange_acct in secrets.EXCHANGE_ACCOUNTS:
-            SyncRunner.sync(
-                secrets.GOOGLE_SERVICE_ACCOUNT_FILE,
-                exchange_acct['emailAddress'],
-                exchange_acct['password'],
-                exchange_acct['server'],
-                exchange_acct['googleCalendarId']
-            )
-        sleep(1800)
-
+    for exchange_acct in secrets.EXCHANGE_ACCOUNTS:
+        SyncRunner.sync(
+            secrets.GOOGLE_SERVICE_ACCOUNT_FILE,
+            exchange_acct['emailAddress'],
+            exchange_acct['password'],
+            exchange_acct['server'],
+            exchange_acct['googleCalendarId']
+        )
